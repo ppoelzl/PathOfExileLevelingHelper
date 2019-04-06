23f939c444c62db54f39dcd7cc97919b03d5e442
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2019-04-06 23:17:39 +0200

    Updated deprecated validator.

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired
 
 
 class ImportForm(FlaskForm):
     import_code = StringField(
         "Input Path of Building Import Code or Pastebin.com link:",
-        validators=[DataRequired()],
+        validators=[InputRequired()],
     )
     submit = SubmitField("Calculate skill gems")
